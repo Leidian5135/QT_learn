25d7c260e37cd02e024c26d8d779dff5174ad767
leidian5135 <944835208@qq.com>
2023-12-15 16:18:53 +0800

    Changes

---
+++ src/controls/QuitApplication.py
@@ -0,0 +1,35 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget, QWidget, QHBoxLayout, QPushButton
+
+
+class QuitApplication(QMainWindow):
+    def __init__(self):
+        super(QuitApplication, self).__init__()
+        self.resize(300, 200)
+        self.setWindowTitle('退出应用程序')
+
+        # 添加Button
+        self.button1 = QPushButton('退出', self)
+        # 将信号与槽关联
+        self.button1.clicked.connect(self.onClick_Button)
+
+        layout = QHBoxLayout()
+        layout.addWidget(self.button1)
+
+        mainFrame = QWidget()
+        mainFrame.setLayout(layout)
+
+        self.setCentralWidget(mainFrame)
+
+    def onClick_Button(self):
+        sender = self.sender()
+        print(sender.text() + ' 按钮被按下')
+        app = QApplication.instance()
+        # 推出应用程序
+        app.quit()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = QuitApplication()
+    main.show()
+    sys.exit(app.exec_())

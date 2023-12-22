11ad1f7cb1ae1a3716f484692d5ca84f405a7a0c
leidian5135 <944835208@qq.com>
2023-12-22 11:04:57 +0800

    Changes

--- src/printer/PrintSupport.py
+++ src/printer/PrintSupport.py
@@ -10,14 +10,15 @@ from PyQt5 import QtGui, QtWidgets, QtPrintSupport
 from PyQt5.QtWidgets import *
 import sys
 
+
 class PrintSupport(QMainWindow):
     def __init__(self):
-        super(PrintSupport,self).__init__()
+        super(PrintSupport, self).__init__()
         self.setGeometry(500, 200, 300, 300)
-        self.button = QPushButton('打印QTextEdit控件中的内容',self)
-        self.button.setGeometry(20,20,260,30)
-        self.editor = QTextEdit('默认文本',self)
-        self.editor.setGeometry(20,60,260,200)
+        self.button = QPushButton('打印QTextEdit控件中的内容', self)
+        self.button.setGeometry(20, 20, 260, 30)
+        self.editor = QTextEdit('默认文本', self)
+        self.editor.setGeometry(20, 60, 260, 200)
 
         self.button.clicked.connect(self.print)
 
@@ -28,7 +29,7 @@ class PrintSupport(QMainWindow):
         # 将绘制的目标重定向到打印机
         painter.begin(printer)
         screen = self.editor.grab()
-        painter.drawPixmap(10,10,screen)
+        painter.drawPixmap(10, 10, screen)
         painter.end()
         print("print")
 
